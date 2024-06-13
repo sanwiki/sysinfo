9951adb6da039349394fb4e3e41b2bbcb057495f
sanwiki <santoshwiki@gmail.com>
2024-06-13 22:18:36 +0530

    IPv6 Update 2024-06-13_22-18-36

--- rishiip.sh
+++ rishiip.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 # Using curl to get public IP from ifconfig.me
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+
 public_ip=$(curl -s ifconfig.me)
 
-echo "$public_ip"
+file_path="/home/sam/githubproject/sysinfo/risi.ip"
+
+# Write the value to the file
+echo "$public_ip" > "$file_path"
+echo "Current timestamp: $timestamp"
+
+git add .
+git commit -m "IPv6 Update $timestamp" 
+git push main origin main
